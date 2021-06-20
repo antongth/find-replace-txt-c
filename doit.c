243e70798c320b7a5a9288f42fb34b474776cd64
antongth <antongth@gmail.com>
2021-06-20 20:44:21 +0600

    second stage of dev

--- doit.c
+++ doit.c
@@ -4,97 +4,135 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
-#define SIZE  512
+#define SIZE 1024
 
-int main(int argc, char* argv[])                  
-{   
-    DIR * d;
-    FILE* f;
-    FILE* f1;
-    char sbuffer[SIZE];
-    char* fn;
+char* txtToFnd = NULL;
+char* txtToRepls = NULL;
+char sBuff[SIZE];
 
-    char* fd = argv[1];
-    char* t = argv[2];
-    char* tr = argv[3];
-    unsigned int ts = strlen(t);
-    unsigned int trs = strlen(tr);
+void findandrepl(FILE*, FILE*, char*);
+void findandrepleq(FILE* ptrFile);
 
-    if ((d = opendir(fd)) == NULL) {
-        perror(fd);
+int main(int argc, char* argv[]) {
+
+    DIR* ptrDir = NULL;
+    struct dirent* dirEnt = NULL;
+    FILE* ptrFile = NULL;
+    FILE* ptrFileTmp = NULL;
+    char fName[SIZE];//char* ffName = malloc(strlen(dir) + strlen(fName) + 2);
+    unsigned short fType;
+    char* dir = NULL;
+
+    //debug
+    char a1[] = ".\\fd";
+    char a2[] = "zz";
+    char a3[] = "xx";
+    dir = a1;
+    txtToFnd = a2;
+    txtToRepls = a3;
+
+    if ((argc != 4)) {
+        perror("invalid number of args"); 
+        return 0;
+    }
+    
+    if (strcmp(txtToFnd, txtToRepls) == 0) {
+        return 0;
+    }
+
+    //dir = argv[1];
+    //txtToFnd = argv[2];
+    //txtToRepls = argv[3];
+
+    if ((ptrDir = opendir(dir)) == NULL) {
+        perror(dir);
         return 1;
     }
-    strcat(fd, "\\");
-    struct dirent * dire;
-    while ((dire = readdir(d)) != NULL) {
-        fn = dire -> d_name;
-        
-        char *ffn = malloc(strlen(fd) + strlen(fn) + 1);
-        snprintf(ffn, sizeof(ffn), "%s\\%s", fd, fn);
-        //strcpy(ffn, fd);
-        //strcat(ffn, fn);
-        printf ("%s\n", ffn);
-        
-        if ((f = fopen(ffn, "rt+")) == NULL) {
-            perror(ffn);
+
+    //strcat(dir, "\\");
+  
+    while ((dirEnt = readdir(ptrDir)) != NULL) {
+
+        if ((fType = dirEnt -> d_type) == 16) 
             continue;
-        }
         
-        f1 = fopen("tmp", "w+");
-        while(!feof (f)) {
-            if ((fgets(sbuffer, sizeof(sbuffer), f)) != NULL) {
-                char * strn = strstr(sbuffer, t);
-                if (strn != NULL) {
-                    printf("find %d \n", strn - sbuffer - 1);
-                    printf("suffics %s \n", strn + ts);
-                    char * prefstr = strtok(sbuffer, t);
-                    printf("preffics %s \n", prefstr);
-                    //printf("%s\n", strncat("", sbuffer, strn - sbuffer - 1));
-                    fprintf(f1, "%s%s%s", prefstr, tr, strn + ts);
-                    //fputs(strn, f1);
-                    //printf("p - %d", ftell(f));
-                    //fseek(f, strlen(sbuffer), 1);
-                    //fseek(f, strn - sbuffer - 1, 1);
-                    //fputc("V");
-                    //fseek(f, strlen(strn), 1);
-                } else fprintf(f1, sbuffer);
+        snprintf(fName, sizeof(fName), "%s\\%s", dir, dirEnt -> d_name);
+        //strcpy(fName, dir);
+        //strcat(fName, dirEnt -> d_name);
+        
+        if (strlen(txtToFnd) == strlen(txtToRepls)) {
+            if ((ptrFile = fopen(fName, "r+")) == NULL) {
+                perror(fName); 
+                continue;
             }
+            findandrepleq(ptrFile);
+            fclose(ptrFile);
+        } else {
+            if ((ptrFile = fopen(fName, "r")) == NULL) {
+                perror(fName); 
+                continue;
+            }
+            if ((ptrFileTmp = fopen("tmp", "w+")) == NULL) {
+                perror("error on tmp creaton");
+                return 1;
+            }
+            findandrepl(ptrFileTmp, ptrFile, fName);
+            fclose(ptrFile);
+            fclose(ptrFileTmp);
         }
-        size_t num;
-        freopen("tmp", "r", f1);
-        freopen(ffn, "w", f);
-        while((num = fread(sbuffer, sizeof(char), SIZE, f1)) > 0) {
-            fwrite(sbuffer, sizeof(char), num, f);
-        }
-        fclose(f1);
-        fclose(f);
     }
-    closedir (d);
 
-    
-    
+    closedir (ptrDir);
+    return 0;                   
+}
 
-    /*while(!feof (f)) {
-        if ((fgets(sbuffer, sizeof(sbuffer), f)) != NULL) {
-            char * strn = strstr(sbuffer, t);
-            if (strn != NULL) {
-                printf("%d \n", strn - sbuffer - 1);
-                printf("%s \n", strn + ts);
+void findandrepl(FILE* ptrFileTmp, FILE* ptrFile, char* fName) {
+
+    char * strn;
+    size_t num;
+    
+    while(!feof (ptrFile)) {
+        if ((fgets(sBuff, sizeof(sBuff), ptrFile)) != NULL) {
+            if ((strn = strstr(sBuff, txtToFnd)) != NULL) {
+                printf("find %d \n", strn - sBuff - 1);
+                printf("suffics %s \n", strn + strlen(txtToFnd));
+                char * prefstr = strtok(sBuff, txtToFnd);
+                printf("preffics %s \n", prefstr);
+                //printf("%s\n", strncat("", sBuff, strn - sBuff - 1));
+                fprintf(ptrFileTmp, "%s%s%s", prefstr, txtToRepls, strn + strlen(txtToFnd));
+                //fputs(strn, ptrFileTmp);
                 //printf("p - %d", ftell(f));
-                //fseek(f, strlen(sbuffer), 1);
-                //fseek(f, strn - sbuffer - 1, 1);
+                //fseek(f, strlen(sBuff), 1);
+                //fseek(f, strn - sBuff - 1, 1);
                 //fputc("V");
                 //fseek(f, strlen(strn), 1);
+            } else fprintf(ptrFileTmp, sBuff);
+        }
+    }
+
+    freopen("tmp", "r", ptrFileTmp);
+    freopen(fName, "w", ptrFile);
+
+    while((num = fread(sBuff, sizeof(char), SIZE, ptrFileTmp)) > 0) {
+        fwrite(sBuff, sizeof(char), num, ptrFile);
+    }
+}
+
+void findandrepleq(FILE* ptrFile) {
+
+    char * strn;
+
+    while(!feof (ptrFile)) {
+        if ((fgets(sBuff, sizeof(sBuff), ptrFile)) != NULL) {
+            strn = strstr(sBuff, txtToFnd);
+            if (strn != NULL) {
+                fseek(ptrFile, strlen(sBuff), 1);
+                fseek(ptrFile, strn - sBuff - 1, 1);
+                fputs(txtToRepls, ptrFile);
+                fseek(ptrFile, strlen(strn), 1);
             }
         }
-    }*/
-
-    //fseek(f, n - str - 1, 0);
-    //fwrite(tr, sizeof(tr), 1, f);
-
-    //printf("%d \n", ts);
-    //printf("%d \n", sizeof(char));
-    printf("End! \n");
-    //fclose(f);
-    return 0;                   
+    }
+    //fseek(ptrFile, n - txtToRepls - 1, 0);
+    //fwrite(txtToRepls, sizeof(txtToRepls), 1, ptrFile);
 }
